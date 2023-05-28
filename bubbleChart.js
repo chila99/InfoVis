a690fc753b920330202c32dbb448678756c051fb
Davide Molitierno <davidmolitiern@hotmail.it>
2023-05-28 11:55:34 +0200

    Aggiunte etichette con transizione al click Aggiunte label on mouse over sui cerchi che identificano variabile relativa alla grandezza del cerchio

--- bubbleChart.js
+++ bubbleChart.js
@@ -1,12 +1,9 @@
-var margin = {top: 30, right: 50, bottom: 30, left: 50};
+const margin = {top: 30, right: 50, bottom: 30, left: 50};
 
 var svg = d3.select("svg");
 
-width = window.innerWidth - margin.left - margin.right;
-height = 700 - margin.top - margin.bottom;
-
-margin_bottom = 100 + height;
-margin_top = 50;
+const width = window.innerWidth - margin.left - margin.right;
+const height = 700 - margin.top - margin.bottom;
 
 var xScale = d3.scaleLinear()
     .domain([0, width])
@@ -23,6 +20,10 @@ var rScale = d3.scaleLinear()
 
 var xAxis = d3.axisBottom(xScale).ticks(10);  		// Bottom = ticks below
 var yAxis = d3.axisLeft(yScale).ticks(10);   // Left = ticks on the left 
+
+var current_x = "v1";
+var current_y = "v2";
+var current_r = "v3";
 
 function updateXScaleDomain(data) {
     xScale.domain([0, d3.max(data, function(d) { return d.v1; }) + 5]);
@@ -44,13 +45,7 @@ function updateDrawing(data) {
     // create svg and circles elements
     var svg = d3.select("svg");
     var circles = svg.selectAll("circle")
-        .data(data)
-        .attr("cx", function(d) { return xScale(d.v1); })
-        .attr("cy", function(d) { return yScale(d.v2); })
-        .attr("r", function(d) { return rScale(d.v3); })
-        .attr("fill", function(d) { return d3.rgb(d.v1 * 255 / max_v1, d.v2 * 255 / max_v2, d.v3 * 255 / max_v3); })
-        .attr("stroke", "black")
-        .attr("stroke-width", 1);
+        .data(data);
 
     circles.enter().append("circle")
         .attr("cx", function(d) { return xScale(d.v1); })
@@ -58,17 +53,30 @@ function updateDrawing(data) {
         .attr("r", function(d) { return rScale(d.v3); })
         .attr("fill", function(d) { return d3.rgb(d.v1 * 255 / max_v1, d.v2 * 255 / max_v2, d.v3 * 255 / max_v3); })
         .attr("stroke", "black")
-        .attr("stroke-width", 1);
+        .attr("stroke-width", 3)
+        .on("mouseover", function(d) {
+            // get the circles coordinates, and add the value as text
+            const r = d3.select(this).attr("r");
+            const x = d3.select(this).attr("cx");
+            const y = d3.select(this).attr("cy");
 
+            svg.append("text")
+            .attr("class", "value")
+            .attr("x", x)
+            .attr("y", y)
+            .attr("text-anchor", "middle")
+            .text(parseFloat(r).toFixed(2));    
+          })
+          .on("mouseout", function() {
+            // Remove v3 value and its background on mouseout
+            svg.selectAll(".value, .value-background").remove();
+            });
     circles.exit().remove();
 }
 
 function drawAxes(data) {
 
     var svg = d3.select("svg");
-
-    console.log(data[0]);
-
     margin_bottom = margin.bottom + height;
     // draw the x-axis
     svg.append("g")
@@ -79,8 +87,15 @@ function drawAxes(data) {
                 temp = data[i]["v1"];
                 data[i]["v1"] = data[i]["v2"];
                 data[i]["v2"] = temp;
-                redraw(data);
             }
+            // sort the data by v3
+            data.sort(function(a, b) {
+                return -(a.v3 - b.v3);
+            });
+            temp = current_x;
+            current_x = current_r;
+            current_r = temp;
+            redraw(data);
         })  
         .call(xAxis);
 
@@ -93,38 +108,68 @@ function drawAxes(data) {
                 temp = data[i]["v1"];
                 data[i]["v1"] = data[i]["v3"];
                 data[i]["v3"] = temp;
-                redraw(data);
             }
+            // sort the data by v3
+            data.sort(function(a, b) {
+                return -(a.v3 - b.v3);
+            });
+            temp = current_y;
+            current_y = current_r;
+            current_r = temp;
+            console.log(current_y);
+            redraw(data);
         })
         .call(yAxis);
+        
+    // add a label along the x-axis
+    svg.append("text")
+        .attr("class", "x label")
+        .attr("y", margin_bottom - 15)
+        .attr("x", width + margin.left - 15)
+        .attr("font-size","15px")
+        .style("text-anchor", "end")
+        .text(current_x);
+
+    // add a label along the y-axis
+    svg.append("text")
+        .attr("class", "y label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 15)
+        .attr("x", -margin.top)
+        .attr("font-size","15px")
+        .style("text-anchor", "end")
+        .text(current_y);        
+
 }
 
 function redraw(data) {
     var svg = d3.select("svg");
-
-    // transition for the change in axes
-    var max_v1 = d3.max(data, function(d) { return d.v1; });
-    var max_v2 = d3.max(data, function(d) { return d.v2; });
-    var max_v3 = d3.max(data, function(d) { return d.v3; });
-
     updateXScaleDomain(data);
     updateYScaleDomain(data);
     updateRScaleDomain(data);
     
-    var xAxis = d3.transition().duration(1000).select(".x.axis").call(d3.axisBottom(xScale).ticks(10));  		// Bottom = ticks below
-    var yAxis = d3.transition().duration(1000).select(".y.axis").call(d3.axisLeft(yScale).ticks(10));   // Left = ticks on the left
-    var rAxis = d3.transition().duration(1000).select(".r.axis").call(d3.axisLeft(rScale).ticks(10));   // Left = ticks on the left
+    d3.transition().duration(1000).select(".x.axis").call(d3.axisBottom(xScale).ticks(10));  		// Bottom = ticks below
+    d3.transition().duration(1000).select(".y.axis").call(d3.axisLeft(yScale).ticks(10));   // Left = ticks on the left
+    d3.transition().duration(1000).select(".r.axis").call(d3.axisLeft(rScale).ticks(10));   // Left = ticks on the left
 
-    var circles = svg.selectAll("circle")
+    svg.selectAll("circle")
         .transition()
         .duration(1000)
         .attr("cx", function(d) { return xScale(d.v1); })
         .attr("cy", function(d) { return yScale(d.v2); })
         .attr("r", function(d) { return rScale(d.v3); });
+
+    // transition for the change in labels
+    d3.transition().duration(1000).select(".x.label").text(current_x);
+    d3.transition().duration(1000).select(".y.label").text(current_y);
 }
 
 d3.json("dataset/trivariate.json")
     .then(function(data) {
+        // sort the data by v3
+        data.sort(function(a, b) {
+            return -(a.v3 - b.v3);
+        });
         updateXScaleDomain(data);
         updateYScaleDomain(data);
         updateRScaleDomain(data);
